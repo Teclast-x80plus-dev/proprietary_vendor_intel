66700b4d11664ee065c54a08a54c988ad8dc6ae5
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-18 16:22:26 +0800

    fix derp
    Change-Id: Ife6a1b62bf95b44a78959d4d2dd2ffe7fc099893

--- cherrytrail-common/Android.mk
+++ cherrytrail-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter chuwi_vi10plus cube_iwork8ultimate,$(TARGET_DEVICE)),)
+ifneq ($(filter x80plus_h5c5,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := itux
